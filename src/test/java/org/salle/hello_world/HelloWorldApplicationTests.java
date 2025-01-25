b0e326d0a711375785072f20841137a4fed723d9
Iván Jimeno Ramírez <ivan.jimeno@seniordomo.es>
2025-01-25 22:44:53 +0100

    test(spring): add main method usage

--- src/test/java/org/salle/hello_world/HelloWorldApplicationTests.java
+++ src/test/java/org/salle/hello_world/HelloWorldApplicationTests.java
@@ -3,7 +3,7 @@ package org.salle.hello_world;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(useMainMethod = SpringBootTest.UseMainMethod.ALWAYS)
 class HelloWorldApplicationTests {
 
 	@Test
