b5b731ba0dd19005b5575721a2c31cb8360e1022
Iván Jimeno Ramírez <ivan.jimeno@seniordomo.es>
2025-01-25 23:13:07 +0100

    build(test): ignore integration test

---
+++ src/test/java/org/salle/hello_world/HelloWorldSpringTest.java
@@ -0,0 +1,12 @@
+package org.salle.hello_world;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(useMainMethod = SpringBootTest.UseMainMethod.ALWAYS)
+public class HelloWorldSpringTest {
+
+        @Test
+        void contextLoads() {
+        }
+}
