21545b87408a76780e24ee6692ee4616c7005895
Iván Jimeno Ramírez <ivan.jimeno@seniordomo.es>
2025-01-25 20:33:00 +0100

    fix(hello): Include exclamation character

---
+++ untitled/src/Main.java
@@ -0,0 +1,10 @@
+
+/**
+ * Author: Iván Jimeno <ivan.jimeno@seniordomo.es>
+ * Date: 25/01/2025
+ * Time: 20:31
+ */public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
