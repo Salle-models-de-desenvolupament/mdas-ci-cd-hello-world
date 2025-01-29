995f86e5aa92add4ccfa41a0d9dc208be895a64a
Iván Jimeno Ramírez <ivan.jimeno@seniordomo.es>
2025-01-30 00:11:43 +0100

    fix(greeting): update response message

--- src/test/java/org/salle/GreetingResourceIT.java
+++ src/test/java/org/salle/GreetingResourceIT.java
@@ -14,6 +14,6 @@ class GreetingResourceIT {
                 .when().get("/hello")
                 .then()
                 .statusCode(200)
-                .body(is("Hello world!"));
+                .body(is("Hello world"));
     }
 }
