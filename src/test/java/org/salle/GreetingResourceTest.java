0c0418dd03340d7c88bd6e5f7e4e18e45ce69cf2
Iván Jimeno Ramírez <ivan.jimeno@seniordomo.es>
2025-01-30 00:52:00 +0100

    add test

--- src/test/java/org/salle/GreetingResourceTest.java
+++ src/test/java/org/salle/GreetingResourceTest.java
@@ -6,4 +6,12 @@ import org.junit.jupiter.api.Test;
 
 @QuarkusTest
 class GreetingResourceTest {
+
+    @Test
+    void testHelloEndpoint() {
+        GreetingResource greetingResource = new GreetingResource();
+        String response = greetingResource.hello();
+        Assertions.assertThat(response).isEqualTo("Hello world");
+    }
+
 }
